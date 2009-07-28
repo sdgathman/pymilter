c29a21d2dd3ffa849319fa73cf89bad6fe4d9204
Stuart Gathman <stuart@gathman.org>
2009-07-28 22:13:46 +0000

    Document getdiag, getversion.

--- doc/milter.py
+++ doc/milter.py
@@ -37,7 +37,14 @@ def set_exception_policy(code): pass
 def register(name,negotiate=None,unknown=None,data=None): pass
 def opensocket(rmsock): pass
 def main(): pass
+
+## Set the libmilter debugging level.
+# smfi_setdbg sets the milter library's internal debugging level to a new level
+# so that code details may be traced. A level of zero turns off debugging. The
+# greater (more positive) the level the more detailed the debugging. Six is the
+# current, highest, useful value.
 def setdbg(lev): pass
+
 def settimeout(secs): pass
 def setbacklog(n): pass
 
@@ -57,3 +64,24 @@ def setconn(s): pass
 
 ## Stop the milter gracefully.
 def stop(): pass
+
+## Retrieve diagnostic info.
+# Return a tuple with diagnostic info gathered by the milter module.
+# The first two fields are counts of milterContext objects created
+# and deleted.  Additional fields may be added later.
+# @return a tuple of diagnostic data
+def getdiag(): pass
+
+## Retrieve the runtime libmilter version.
+# Return the runtime libmilter version. This can be different
+# from the compile time version when sendmail or libmilter is upgraded
+# after pymilter is compiled.
+# @return a tuple of <code>(major,minor,patchlevel)</code>
+def getversion(): pass
+
+## The compile time libmilter version.
+# Python code might need to deal with pymilter compiled 
+# against various versions of libmilter.  This module constant 
+# contains the contents of the <code>SMFI_VERSION</code> macro when
+# the milter module was compiled.
+VERSION = 0x1000001
