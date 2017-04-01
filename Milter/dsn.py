a647ae8df324e3aa2c781faca4a0ab4c2fb37d9b
William <traderwilliam@gmail.com>
2017-04-01 13:49:39 +0800

    Fix some syntax errors to work with python3

--- Milter/dsn.py
+++ Milter/dsn.py
@@ -142,13 +142,13 @@ def send_dsn(mailfrom,receiver,msg=None,timeout=600,session=None,ourfrom=''):
         if badrcpts:
           return badrcpts
       return None			# success
-    except smtplib.SMTPRecipientsRefused,x:
+    except smtplib.SMTPRecipientsRefused as x:
       if len(x.recipients) == 1:
         return x.recipients.values()[0]	# permanent error
       return x.recipients
-    except smtplib.SMTPSenderRefused,x:
+    except smtplib.SMTPSenderRefused as x:
       return x.args[:2]			# does not accept DSN
-    except smtplib.SMTPDataError,x:
+    except smtplib.SMTPDataError as x:
       return x.args			# permanent error
     except smtplib.SMTPException:
       pass		# any other error, try next MX
