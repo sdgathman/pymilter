ec78eed275d089eb88dd20cc27e3ab52b3b09720
Ralph Seichter <github@seichter.de>
2019-04-17 13:35:38 +0200

    Fix for compilation error on macOS 10.14 (Mojave)
    This change ensures that arpa/inet.h is included when building
    miltermodule.c on macOS 10.14. See
    https://github.com/sdgathman/pymilter/issues/30
    
    Signed-off-by: Ralph Seichter <github@seichter.de>

--- miltermodule.c
+++ miltermodule.c
@@ -71,7 +71,7 @@ $ python setup.py help
  * published.  Unfortunately I know of no good way to do this
  * other than with OS-specific tests.
  */
-#if defined(__FreeBSD__) || defined(__linux__) || defined(__sun__) || defined(__GLIBC__)
+#if defined(__FreeBSD__) || defined(__linux__) || defined(__sun__) || defined(__GLIBC__) || (defined(__APPLE__) && defined(__MACH__))
 #define HAVE_IPV6_RFC2553
 #include <arpa/inet.h>
 #endif
